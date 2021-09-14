076f7e788d3d9062562ec8b1e8f1ac289a7120a9
coil <75682926+ilsung12@users.noreply.github.com>
2021-09-15 00:42:01 +0900

    📝 Learn : Controlled Component

--- react-fc/CH02/test/my-app/src/App.js
+++ react-fc/CH02/test/my-app/src/App.js
@@ -3,14 +3,18 @@
 // import ClassComponent from './components/2-5.State/ClassComponent';
 // import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 // import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent2';
-import Event from './components/2-7.Event/Event';
-import Condition from './components/2-8.ConditionalRendering/Condition';
-import List from './components/2-9.List/List';
+import ControlledComponent from './components/2-10.Form/ControlledComponent';
+import UncontrolledComponent from './components/2-10.Form/UncontrolledComponent';
+//import Event from './components/2-7.Event/Event';
+//import Condition from './components/2-8.ConditionalRendering/Condition';
+//import List from './components/2-9.List/List';
 
 function App() {
   return (
     <div className="App">
-      <List />
+      <ControlledComponent />
+      <UncontrolledComponent />
+      {/* <List /> */}
       {/* <Condition /> */}
       {/* <Event /> */}
       {/* <ClassComponent2 /> */}
